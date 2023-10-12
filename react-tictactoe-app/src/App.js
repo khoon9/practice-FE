353c5a29c4ea41e54174c61300e5dec43d715ea6
kshoon99 <kshun08@dongguk.edu>
2023-10-13 00:44:58 +0900

    feat: add the contents practiced in lectures 05(about react components)

--- react-tictactoe-app/src/App.js
+++ react-tictactoe-app/src/App.js
@@ -1,28 +1,34 @@
 import { Component } from "react";
 import "./App.css";
+import ExpenseForm from "./components/ExpenseForm";
+import ExpenseList from "./components/ExpenseList";
 
-class App extends Component{
+class App extends Component {
   render() {
     return (
-      <main className = "main-container">
-        <h1>
-          예산 계산기
-        </h1>
-        <div style={{width: '100%', backgroundColor: 'white', padding:'1rem'}}>
+      <main className="main-container">
+        <h1>예산 계산기</h1>
+
+        <div
+          style={{ width: "100%", backgroundColor: "white", padding: "1rem" }}
+        >
+          <ExpenseForm />
         </div>
-        <div style={{width: '100%', backgroundColor: 'white', padding:'1rem'}}>
+        <div
+          style={{ width: "100%", backgroundColor: "white", padding: "1rem" }}
+        >
+          <ExpenseList />
         </div>
-        <div style={{display: 'flex', justifyContent: 'end', marginTop: '1rem'}}>
-            <p style={{fontSize: '2rem'}}>
-              총지출:
-              <span>
-                원
-              </span>
-            </p>
+        <div
+          style={{ display: "flex", justifyContent: "end", marginTop: "1rem" }}
+        >
+          <p style={{ fontSize: "2rem" }}>
+            총지출:
+            <span>원</span>
+          </p>
         </div>
       </main>
-    )
-  
+    );
   }
 }
 export default App;
---
+++ react-tictactoe-app/src/components/ExpenseList.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+import ExpenseItem from "./ExpenseItem";
+
+export class ExpenseList extends Component {
+  render() {
+    return (
+      <div>
+        <ul className="list">{<ExpenseItem />}</ul>
+        <button className="btn">목록 지우기</button>
+      </div>
+    );
+  }
+}
+
+export default ExpenseList;
