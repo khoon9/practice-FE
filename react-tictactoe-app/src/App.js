0eee629792cef10ac0dfe9bdcd21ea0fc03e651e
kshoon99 <kshun08@dongguk.edu>
2023-10-12 19:22:35 +0900

    feat: add the contents practiced in lectures 04(about react)

---
+++ react-tictactoe-app/src/App.js
@@ -0,0 +1,28 @@
+import { Component } from "react";
+import "./App.css";
+
+class App extends Component{
+  render() {
+    return (
+      <main className = "main-container">
+        <h1>
+          예산 계산기
+        </h1>
+        <div style={{width: '100%', backgroundColor: 'white', padding:'1rem'}}>
+        </div>
+        <div style={{width: '100%', backgroundColor: 'white', padding:'1rem'}}>
+        </div>
+        <div style={{display: 'flex', justifyContent: 'end', marginTop: '1rem'}}>
+            <p style={{fontSize: '2rem'}}>
+              총지출:
+              <span>
+                원
+              </span>
+            </p>
+        </div>
+      </main>
+    )
+  
+  }
+}
+export default App;
