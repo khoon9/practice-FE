fb5fb2e94fbb5ae8255e8bd77dcfab57a879a68c
kshoon99 <kshun08@dongguk.edu>
2023-10-13 16:43:13 +0900

    feat: add the contents practiced in lectures 06(about react props)

--- react-tictactoe-app/src/App.js
+++ react-tictactoe-app/src/App.js
@@ -4,6 +4,11 @@ import ExpenseForm from "./components/ExpenseForm";
 import ExpenseList from "./components/ExpenseList";
 
 class App extends Component {
+  initialExpenses = [
+    { id: 1, charge: "렌트비", amount: 1600 },
+    { id: 2, charge: "교통비", amount: 400 },
+    { id: 3, charge: "식비", amount: 1200 },
+  ];
   render() {
     return (
       <main className="main-container">
@@ -17,7 +22,7 @@ class App extends Component {
         <div
           style={{ width: "100%", backgroundColor: "white", padding: "1rem" }}
         >
-          <ExpenseList />
+          <ExpenseList initialExpenses={this.initialExpenses} />
         </div>
         <div
           style={{ display: "flex", justifyContent: "end", marginTop: "1rem" }}
--- react-tictactoe-app/src/components/ExpenseForm.js
+++ react-tictactoe-app/src/components/ExpenseForm.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import "./ExpenseForm.css";
-
+import { MdSend } from "react-icons/md";
 export class ExpenseForm extends Component {
   render() {
     return (
@@ -29,6 +29,7 @@ export class ExpenseForm extends Component {
         </div>
         <button type="submit" className="btn">
           제출
+          <MdSend className="btn-icon" />
         </button>
       </form>
     );
