ee06d80b7ee5af1f4ad3bb00ace8a49dd36abda7
kshoon99 <kshun08@dongguk.edu>
2023-10-22 19:20:17 +0900

    feat: add the contents practiced in lectures 34(about pokemon detail page 02)

--- react-pokemon-app/src/pages/DetailPage/index.jsx
+++ react-pokemon-app/src/pages/DetailPage/index.jsx
@@ -1,8 +1,11 @@
 import axios from "axios";
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 
 const DetailPage = () => {
+  const [pokemon, setPokemon] = useState();
+  const [isLoading, setIsLoading] = useState(true);
+
   const params = useParams();
   const pokemonId = params.id;
   const baseUrl = "https://pokeapi.co/api/v2/pokemon/";
@@ -21,9 +24,33 @@ const DetailPage = () => {
         const { name, id, types, weight, height, stats, abilities } =
           pokemonData;
         const nextAndPreviousPokemon = await getNextAndProviousPokemon(id);
-        console.log(nextAndPreviousPokemon);
+
+        // return : 각 map 의 return 에 대해 모두 기다린 후 한번에 반환
+        // map 에 의해 배열로 리턴됨.
+        const DamageRelations = await Promise.all(
+          types.map(async (i) => {
+            const type = await axios.get(i.type.url);
+            return type.data.damage_relations;
+          })
+        );
+        console.log("D", DamageRelations);
+
+        const formattedPokemonData = {
+          id,
+          name,
+          weight: weight / 10,
+          height: height / 10,
+          previous: nextAndPreviousPokemon.previous,
+          next: nextAndPreviousPokemon.next,
+          abilities: formatPokemonAlilities(abilities),
+          stats: formatPokemonStats(stats),
+          DamageRelations,
+        };
+        setPokemon(formattedPokemonData);
+        setIsLoading(false);
+
+        console.log(formattedPokemonData);
       }
-      console.log(pokemonData);
     } catch (error) {
       console.log(error);
     }
@@ -32,7 +59,6 @@ const DetailPage = () => {
   async function getNextAndProviousPokemon(id) {
     const urlPokemon = `${baseUrl}?limit=1&offset=${id - 1}`;
     const { data: pokemonData } = await axios.get(urlPokemon);
-    console.log("*****", pokemonData);
     const nextResponse =
       pokemonData.next && (await axios.get(pokemonData.next));
     const previoustResponse =
@@ -42,6 +68,28 @@ const DetailPage = () => {
       previous: previoustResponse?.data?.results?.[0]?.name,
     };
   }
+
+  const formatPokemonAlilities = (abilities) => {
+    return abilities
+      .filter((_, index) => index <= 1)
+      .map((obj) => obj.ability.name.replaceAll("-", " "));
+  };
+
+  const formatPokemonStats = ([
+    statHP,
+    statATK,
+    statDEP,
+    statSATK,
+    statSDEP,
+    statSPD,
+  ]) => [
+    { name: "Hit Points", baseStat: statHP.base_stat },
+    { name: "Attack", baseStat: statATK.base_stat },
+    { name: "Defense", baseStat: statDEP.base_stat },
+    { name: "Special Attack", baseStat: statSATK.base_stat },
+    { name: "Special Defense", baseStat: statSDEP.base_stat },
+    { name: "Speed", baseStat: statSPD.base_stat },
+  ];
 
   return <div>DetailPage</div>;
 };
