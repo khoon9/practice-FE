c50c1ce3e941f46bc3bd09a8e10ffab611da81d9
kshoon99 <kshun08@dongguk.edu>
2023-10-26 00:55:08 +0900

    feat: add the contents practiced in lectures 45(about poke login UI)

--- react-pokemon-app/src/App.jsx
+++ react-pokemon-app/src/App.jsx
@@ -1,15 +1,33 @@
 import React from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Outlet } from "react-router-dom";
 import MainPage from "./pages/MainPage";
 import DetailPage from "./pages/DetailPage";
 import "./App.css";
+import LoginPage from "./pages/LoginPage";
+import NavBar from "./components/NavBar";
+
+// react router dom 을 사용
+const Layout = () => {
+  return (
+    <>
+      <NavBar />
+      <br />
+      <br />
+      <br />
+      <Outlet />
+    </>
+  );
+};
 
 const App = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<MainPage />}></Route>
-        <Route path="/pokemon/:id" element={<DetailPage />}></Route>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<MainPage />} />
+          <Route path="login" element={<LoginPage />} />
+          <Route path="/pokemon/:id" element={<DetailPage />} />
+        </Route>
       </Routes>
     </BrowserRouter>
   );
